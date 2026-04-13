ebb7f076e28eb2912365b5b8f6d3aa6627655b78
Darkshadow-dev <ahmedhasid479@gmail.com>
2026-04-13 21:12:43 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -4,6 +4,13 @@ echo "Installing Arduino CLI..."
 
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 
+chmod +x bin/arduino-cli
+
 mv bin/arduino-cli /usr/local/bin/arduino-cli
 
 arduino-cli version
+
+echo "Installing Arduino AVR core..."
+
+arduino-cli core update-index
+arduino-cli core install arduino:avr
