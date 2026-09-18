b37256d2b3d8b19410865baefe8d57e53e61ccc0
Darkshadow-dev <ahmedhasid479@gmail.com>
2026-09-18 16:29:17 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -8,17 +8,27 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 
 export PATH="/opt/render/project/src/bin:$PATH"
 
+echo "Arduino CLI:"
+arduino-cli version
+
 arduino-cli config init --overwrite
 
 arduino-cli core update-index
 
+echo "Installing AVR core..."
 arduino-cli core install arduino:avr
+
+echo "Checking AVR core..."
+arduino-cli core list
 
 arduino-cli lib update-index
 
+echo "Installing libraries..."
 arduino-cli lib install "Adafruit BusIO"
 arduino-cli lib install "Adafruit GFX Library"
 arduino-cli lib install "Adafruit SSD1306"
 
 echo "Installed libraries:"
 arduino-cli lib list
+
+echo "Build environment ready."
